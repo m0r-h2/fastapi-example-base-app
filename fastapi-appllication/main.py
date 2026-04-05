0829fd50eeabd3abee13be1672c2daca06a59c67
Kirll Savhcenko <Savhcenko55533@gmail.com>
2026-04-05 14:28:03 +0300

    include api router

--- fastapi-appllication/main.py
+++ fastapi-appllication/main.py
@@ -3,7 +3,14 @@ import uvicorn
 
 from core.config import settings
 
+from api import router as api_router
+
 app = FastAPI()
+
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
 
 if __name__ == "__main__":
     uvicorn.run(
