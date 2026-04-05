4bba8b4ad9d21c015f88199a5681e76c07e477fc
Kirll Savhcenko <Savhcenko55533@gmail.com>
2026-04-05 18:02:39 +0300

    create database config

--- fastapi-appllication/core/config.py
+++ fastapi-appllication/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings
 from pydantic import BaseModel
+from sqlalchemy import URL
 
 class RunConfig(BaseModel):
     host: str = "127.0.0.1"
@@ -10,9 +11,40 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class SqlalchemyConfig(BaseModel):
+    echo: bool = True
+    echo_pool: bool = False
+    pool_size: int = 5
+    max_overflow: int = 10
+
+class DatabaseConfig(BaseModel):
+    username: str = "user"
+    password: str = "password"
+    database: str ="shop"
+    port: int = 5432
+    host: str = "db"
+    driver_name: str = "postgresql+asyncpg"
+    @property
+    def async_url(self) -> URL:
+        return URL.create(
+            username=self.username,
+            password=self.password,
+            database=self.database,
+            port=self.port,
+            host=self.host,
+            drivername=self.driver_name
+        )
+
+    sqla: SqlalchemyConfig = SqlalchemyConfig()
+
+
+
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
