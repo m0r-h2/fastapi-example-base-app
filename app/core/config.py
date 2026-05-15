77cc31ff7a0a50c1dade47ef9a3628fb2ea1676b
Kirll Savhcenko <Savhcenko55533@gmail.com>
2026-05-15 10:22:45 +0300

    adds config api /departments

--- app/core/config.py
+++ app/core/config.py
@@ -1,38 +1,39 @@
-from dotenv import load_dotenv
+from pydantic import BaseModel, Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel
 from sqlalchemy import URL
-import os
-import dotenv
-
-load_dotenv()
 
 
 class RunConfig(BaseModel):
-    host: str = "127.0.0.1"
-    port: str = 8000
+    host: str = "0.0.0.0"
+    port: int = 8000
+
 
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
+    departments: str = "/departments"
+
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
-    v1: ApiV1Prefix =  ApiV1Prefix()
+    v1: ApiV1Prefix = ApiV1Prefix()
 
 
 class SqlalchemyConfig(BaseModel):
-    echo: bool = True
+    echo: bool = False
     echo_pool: bool = False
     pool_size: int = 5
     max_overflow: int = 10
 
+
 class DatabaseConfig(BaseModel):
-    username: str = os.getenv("APP_CONFIG__DB__USERNAME")
-    password: str = os.getenv("APP_CONFIG__DB__PASSWORD")
-    database: str = os.getenv("APP_CONFIG__DB__DATABASE")
-    port: int = os.getenv("APP_CONFIG__DB__PORT")
+    username: str = "postgres"
+    password: str = "postgres"
+    database: str = "hitalent"
     host: str = "localhost"
+    port: int = 5432
     driver_name: str = "postgresql+asyncpg"
+    sqla: SqlalchemyConfig = SqlalchemyConfig()
+
     @property
     def async_url(self) -> URL:
         return URL.create(
@@ -41,23 +42,18 @@ class DatabaseConfig(BaseModel):
             database=self.database,
             port=self.port,
             host=self.host,
-            drivername=self.driver_name
+            drivername=self.driver_name,
         )
-
-    sqla: SqlalchemyConfig = SqlalchemyConfig()
-
-
-
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
+        env_file_encoding="utf-8",
         case_sensitive=False,
         env_nested_delimiter="__",
-        env_prefix="APP_CONFIG__"
+        env_prefix="APP_CONFIG__",
     )
-
 
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
